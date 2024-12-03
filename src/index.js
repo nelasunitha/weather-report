013801865da83d7464f3d7f3be1db9c15d3d8051
Sunitha Nela <nelasunitha@gmail.com>
2024-12-02 21:39:26 -0800

    adds helper function for change of text color by temperature value
    Co-authored-by: beenishali693 <beenishali693@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,61 +1,61 @@
 const state = {
-    tempValue: 32,
-    increaseTempControlButton: null,
-    decreaseTempControlButton: null,
-    currentTempButton: null,
-  };
+  tempValue: 32,
+  increaseTempControlButton: null,
+  decreaseTempControlButton: null,
+  currentTempButton: null,
+};
 
-  const loadControls = () => {
-    // state.tempValue = document.getElementById('tempValue');
-    state.increaseTempControlButton = document.getElementById('increaseTempControl');
-    state.decreaseTempControlButton = document.getElementById('decreaseTempControl');
-    state.currentTempButton = document.getElementById('currentTempButton')
-  };
+const loadControls = () => {
+  // state.tempValue = document.getElementById('tempValue');
+  state.increaseTempControlButton = document.getElementById(
+    'increaseTempControl'
+  );
+  state.decreaseTempControlButton = document.getElementById(
+    'decreaseTempControl'
+  );
+  state.currentTempButton = document.getElementById('currentTempButton');
+};
 
-  const increaseTemp = () => {
-    ++state.tempValue;
-    document.getElementById('tempValue').textContent = state.tempValue
-    if (state.tempValue >= 80){
-        tempValue.style.color = 'red';
-      } else if (state.tempValue >=70 && state.tempValue <= 79 ) {
-          tempValue.style.color = 'pink';
-      } else if (state.tempValue >=60 && state.tempValue <= 69 ) {
-          tempValue.style.color = 'baby blue';
-      } else if (state.tempValue >=50 && state.tempValue <= 59 ) {
-          tempValue.style.color = 'orange';
-      } else {
-          tempValue.style.color = 'teal';
-      };
-  };
+let temperature;
 
-  const decreaseTemp = () => {
-    --state.tempValue;
-    document.getElementById('tempValue').textContent = state.tempValue
-    if (state.tempValue >= 80){
-        tempValue.style.color = 'red';
-      } else if (state.tempValue >=70 && state.tempValue <= 79 ) {
-          tempValue.style.color = 'pink';
-      } else if (state.tempValue >=60 && state.tempValue <= 69 ) {
-          tempValue.style.color = 'baby blue';
-      } else if (state.tempValue >=50 && state.tempValue <= 59 ) {
-          tempValue.style.color = 'orange';
-      } else {
-          tempValue.style.color = 'teal';
-      };
-  };
+const increaseTemp = () => {
+  ++state.tempValue;
+  document.getElementById('tempValue').textContent = state.tempValue;
+  temperature = state.tempValue;
+  changeColorByTemperature(temperature);
+};
 
-  const registerEventHandlers = () => {
-    loadControls();
-    state.increaseTempControlButton.addEventListener('click', (increaseTemp))
-    state.decreaseTempControlButton.addEventListener('click', (decreaseTemp))
-  };
+const decreaseTemp = () => {
+  --state.tempValue;
+  document.getElementById('tempValue').textContent = state.tempValue;
+  temperature = state.tempValue;
+  changeColorByTemperature(temperature);
+};
 
+const registerEventHandlers = () => {
+  loadControls();
+  state.increaseTempControlButton.addEventListener('click', increaseTemp);
+  state.decreaseTempControlButton.addEventListener('click', decreaseTemp);
+};
 
+const changeColorByTemperature = (temperature) => {
+  if (temperature >= 80) {
+    tempValue.style.color = 'red';
+  } else if (temperature >= 70 && temperature <= 79) {
+    tempValue.style.color = 'purple';
+  } else if (temperature >= 60 && temperature <= 69) {
+    tempValue.style.color = 'baby blue';
+  } else if (temperature >= 50 && temperature <= 59) {
+    tempValue.style.color = 'orange';
+  } else {
+    tempValue.style.color = 'teal';
+  }
+};
 
-    // state.resetButton.addEventListener('click', () => {
-    //   state.parrotCount = 0;
-    //   state.addParrotCountLabel.textContent = state.parrotCount;
-    // });
+// state.resetButton.addEventListener('click', () => {
+//   state.parrotCount = 0;
+//   state.addParrotCountLabel.textContent = state.parrotCount;
+// });
 //  };
 
-  document.addEventListener('DOMContentLoaded', registerEventHandlers);
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
