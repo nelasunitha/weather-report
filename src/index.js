bbfcbc2a6fbaf868cc7cc89dea67b698d78d7a6f
Beenish <beenishali693@gmail.com>
2024-12-03 11:00:22 -0800

    Change City Name with input added
    Co-authored-by: nelasunitha <nelasunitha@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const state = {
   increaseTempControlButton: null,
   decreaseTempControlButton: null,
   currentTempButton: null,
+  landscape:null,
+  headerCityName: null,
+  cityNameInput: null,
 };
 
 const loadControls = () => {
@@ -14,9 +17,17 @@ const loadControls = () => {
     'decreaseTempControl'
   );
   state.currentTempButton = document.getElementById('currentTempButton');
+  state.landscape = document.getElementById('landscape');
+  state.headerCityName = document.getElementById('headerCityName')
+  state.cityNameInput = document.getElementById('cityNameInput')
 };
 
 let temperature;
+
+
+const changeCityName = () => {
+  headerCityName.innerText = state.cityNameInput.value
+};
 
 const increaseTemp = () => {
   ++state.tempValue;
@@ -36,26 +47,28 @@ const registerEventHandlers = () => {
   loadControls();
   state.increaseTempControlButton.addEventListener('click', increaseTemp);
   state.decreaseTempControlButton.addEventListener('click', decreaseTemp);
+  state.cityNameInput.addEventListener('keyup', changeCityName)
 };
 
 const changeColorByTemperature = (temperature) => {
   if (temperature >= 80) {
     tempValue.style.color = 'red';
+    landscape.textContent = `"🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂"` 
   } else if (temperature >= 70 && temperature <= 79) {
     tempValue.style.color = 'purple';
+    landscape.textContent = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷"
   } else if (temperature >= 60 && temperature <= 69) {
-    tempValue.style.color = 'baby blue';
+    tempValue.style.color = 'blue';
+    landscape.textContent = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃"
   } else if (temperature >= 50 && temperature <= 59) {
     tempValue.style.color = 'orange';
+    landscape.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"
   } else {
     tempValue.style.color = 'teal';
+    landscape.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"
   }
 };
 
-// state.resetButton.addEventListener('click', () => {
-//   state.parrotCount = 0;
-//   state.addParrotCountLabel.textContent = state.parrotCount;
-// });
-//  };
+
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
